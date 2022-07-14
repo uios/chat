5f097d831e955ed0560a68b718f184e17f114ab5
Anon ||| Mous <3849997+anoniiimous@users.noreply.github.com>
2022-07-14 17:18:04 -0400

    Update mvc.js

--- app/cdn/js/mvc.js
+++ app/cdn/js/mvc.js
@@ -16,6 +16,7 @@ window.mvc.v
 
     if (root) {
       dom.footer.find('card').dataset.width = "240px";
+      byId('snap').classList.add('bottom-0');
       //webcam.control.stop();
       if(root === "user") {
         byId('avi').classList.add('hide');
@@ -26,6 +27,7 @@ window.mvc.v
     } 
     else {
       dom.footer.find('card').dataset.width = "100%-40px";
+      byId('snap').classList.remove('bottom-0');
       byId('avi').classList.remove('hide');   
       webcam.stream ? null : webcam.control.play();
       resolve(route);
